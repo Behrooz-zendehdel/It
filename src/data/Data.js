fc2585b3ecc1203bf54953dace0be9b2bb3d97d2
behrooz <behrooz.zendedel1992@gmail.com>
2023-03-11 21:57:59 +0330

    update data files

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,10 +1,3 @@
-import image1 from "../assets/HomeImage/1-1.jpg";
-import image2 from "../assets/HomeImage/1-2.png";
-import image3 from "../assets/HomeImage/1-3.jpg";
-import image4 from "../assets/HomeImage/1-4.jpg";
-import image5 from "../assets/HomeImage/1-5.jpg";
-import image6 from "../assets/HomeImage/1-6.jpg";
-
 export const products = [
   {
     id: 1,
@@ -54,32 +47,32 @@ export const products = [
 export const brands = [
   {
     id: 1,
-    img: image1,
+    img: require("../assets/HomeImage/1-1.jpg"),
     alt: "ubiquiti",
   },
   {
     id: 2,
-    img: image2,
+    img: require("../assets/HomeImage/1-2.png"),
     alt: "zyxel",
   },
   {
     id: 3,
-    img: image3,
+    img: require("../assets/HomeImage/1-3.jpg"),
     alt: "cisco",
   },
   {
     id: 4,
-    img: image4,
+    img: require("../assets/HomeImage/1-4.jpg"),
     alt: "legrqand",
   },
   {
     id: 5,
-    img: image5,
+    img: require("../assets/HomeImage/1-5.jpg"),
     alt: "mimosa",
   },
   {
     id: 6,
-    img: image6,
+    img: require("../assets/HomeImage/1-6.jpg"),
     alt: "mikrotik",
   },
 ];
