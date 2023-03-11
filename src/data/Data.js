b1d8d2bb590426fd49de78b033f6e563d9ace43b
behrooz <behrooz.zendedel1992@gmail.com>
2023-03-11 21:50:01 +0330

    added section network-sorted

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,3 +1,10 @@
+import image1 from "../assets/HomeImage/1-1.jpg";
+import image2 from "../assets/HomeImage/1-2.png";
+import image3 from "../assets/HomeImage/1-3.jpg";
+import image4 from "../assets/HomeImage/1-4.jpg";
+import image5 from "../assets/HomeImage/1-5.jpg";
+import image6 from "../assets/HomeImage/1-6.jpg";
+
 export const products = [
   {
     id: 1,
@@ -41,5 +48,66 @@ export const products = [
     img: "",
     productId: 1,
     desc: "راه اندازی کامل زیرساخت برای شرکت های کوچک و بزرگ به همراه براورد قیمت",
+  },
+];
+
+export const brands = [
+  {
+    id: 1,
+    img: image1,
+    alt: "ubiquiti",
+  },
+  {
+    id: 2,
+    img: image2,
+    alt: "zyxel",
+  },
+  {
+    id: 3,
+    img: image3,
+    alt: "cisco",
+  },
+  {
+    id: 4,
+    img: image4,
+    alt: "legrqand",
+  },
+  {
+    id: 5,
+    img: image5,
+    alt: "mimosa",
+  },
+  {
+    id: 6,
+    img: image6,
+    alt: "mikrotik",
+  },
+];
+
+export const networkSorted = [
+  {
+    id: 1,
+    img: require("../assets/HomeImage/2-1.png"),
+    alt: "cable cat 6",
+  },
+  {
+    id: 2,
+    img: require("../assets/HomeImage/2-2.png"),
+    alt: "modem",
+  },
+  {
+    id: 3,
+    img: require("../assets/HomeImage/2-3.png"),
+    alt: "access point",
+  },
+  {
+    id: 4,
+    img: require("../assets/HomeImage/2-4.png"),
+    alt: "switch",
+  },
+  {
+    id: 5,
+    img: require("../assets/HomeImage/2-5.png"),
+    alt: "voip",
   },
 ];
